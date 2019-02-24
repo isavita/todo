b24f7ce74e4e61f2a201ccc5fe887973e9432ada
Aleksandar Dimov <isavitaisa@gmail.com>
2019-02-24 16:42:22 +0000

    Add metrics module

---
+++ lib/todo/metrics.ex
@@ -0,0 +1,20 @@
+defmodule Todo.Metrics do
+  use Task
+
+  @time_step 5
+
+  def start_link(_opts), do: Task.start_link(&loop/0)
+
+  def loop do
+    Process.sleep(:timer.seconds(@time_step))
+    IO.inspect(collect_metrics())
+    loop()
+  end
+
+  defp collect_metrics do
+    [
+      memory_usage: "#{Float.round(:erlang.memory(:total)/ (1024 * 1024), 3)} MB",
+      process_count: :erlang.system_info(:process_count)
+    ]
+  end
+end
--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -1,6 +1,7 @@
 defmodule Todo.System do
   def start_link do
     Supervisor.start_link([
+      Todo.Metrics,
       Todo.ProcessRegistry,
       %{id: Todo.Database, start: {Todo.Database, :start_link, []}},
       %{id: Todo.Cache, start: {Todo.Cache, :start_link, []}},
